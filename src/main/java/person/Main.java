1d3f948d7ff09f0eda468892e5a5756ceeb9b365
Vuong Gia Hy <hyvuonggia@gmail.com>
2021-04-18 11:08:28 +0200

    Add methods into PersonDao class

--- src/main/java/person/Main.java
+++ src/main/java/person/Main.java
@@ -1,7 +1,14 @@
 package person;
 
-public class Main {
-    public static void main(String[] args) {
+import com.github.javafaker.Faker;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
-    }
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
 }
--- src/main/java/person/PersonDao.java
+++ src/main/java/person/PersonDao.java
@@ -1,4 +1,71 @@
 package person;
 
-public class PersonDao {
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RegisterBeanMapper(Person.class)
+public interface PersonDao {
+
+    /**
+     * Create the database table storing {@code Person} objects
+     */
+    @SqlUpdate("""
+    CREATE TABLE persontable (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR ,
+        birthDate DATE ,
+        gender VARCHAR(6),
+        email VARCHAR ,
+        phone VARCHAR ,
+        profession VARCHAR ,
+        married INTEGER(1), -- 0 is false, 1 is true
+        );
+        """)
+    void createPersonTable();
+
+
+//    @SqlUpdate("""
+//    INSERT INTO persontable VALUES (:id, :name, :birthDate, :gender, :email, :phone, :profession, :married);
+//    """)
+//    void insertPerson(@Bind("id") int id, @Bind("name") String name, @Bind("birthDate") LocalDate birthDate,
+//                      @Bind("gender") String gender, @Bind("email") String email, @Bind("phone") String phone,
+//                      @Bind("profession") String profession, @Bind("married") boolean married) ;
+
+    /**
+     *
+     * @param person
+     */
+    @SqlUpdate("""
+    INSERT INTO persontable VALUES (:id, :name, :birthDate, :gender, :email, :phone, :profession, :married);
+    """)
+    void insertPerson(@BindBean Person person);
+
+    /**
+     * Get person with the id specified from the table
+     *
+     * @param id
+     * @return {@code Optional<Person>}
+     */
+    @SqlQuery("""
+    SELECT * FROM persontable WHERE id = :id;
+    """)
+    Optional<Person> getPerson(@Bind("id") int id);
+
+    @SqlUpdate("""
+    DELETE FROM persontable WHERE id = :id;
+    """)
+    void deletePerson(@Bind("id") int id);
+
+    @SqlQuery("""
+    SELECT * FROM persontable ORDER BY id;   
+    """)
+    List<Person> listPersons();
+
 }
